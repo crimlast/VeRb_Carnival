d2efed0bb0a2bf623fdfc01270b073dc84573f6f
KyleTownsend74 <townsend.kyle.kt@gmail.com>
2022-03-11 14:26:15 -0800

    Add test scene load script

--- Assets/Scripts/MenuSceneSelection.cs
+++ Assets/Scripts/MenuSceneSelection.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuSceneSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadTestScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("TestScene - Copy 1");
     }
 }
