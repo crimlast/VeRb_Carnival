6d3fdab10b59699220241955c20929ab4f4dccc4
crimlast <josh.crimlast23@gmail.com>
2022-03-26 14:56:14 -0700

    BallonPop Script fix

---
+++ Assets/Scripts/BallonPop Specific scripts/BackandForthMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftAndRight : MonoBehaviour
+{
+    float MoveSpeed;
+    Vector3 pointA;
+    Vector3 pointB;
+    void Start()
+    {
+        MoveSpeed = Random.Range(0.2f, 1.5f);
+        pointA = transform.position;
+        pointB = new Vector3(transform.position.x + 2, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Moves the object forward at 5 units per second.
+        float time = Mathf.PingPong(Time.time * MoveSpeed, 1);
+        transform.position = Vector3.Lerp(pointA, pointB, time);
+
+    }
+}
--- Assets/Scripts/BallonPop Specific scripts/BallonCollisionDestroy.cs
+++ Assets/Scripts/BallonPop Specific scripts/BallonCollisionDestroy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionDestroy : MonoBehaviour
+public class BallonCollisionDestroy : MonoBehaviour
 {
     // Static score tracker reference to avoid creating reference for each bullet instance
     private static bool isTrackerSet = false;
@@ -20,20 +20,17 @@ public class CollisionDestroy : MonoBehaviour
         // Destroy the target if hit
         if(collision.gameObject.tag == "Target")
         {
+            // Destroys "target when collided with this object
             Destroy(collision.gameObject);
+
+            //Spawns a screaming mole after target destruction on the same spot
             spawnPos = transform.position;
             molePrefab = Instantiate(molePrefab);
             molePrefab.transform.position = spawnPos;
+            //Destroys the falling mole after 3 seconds.
             Destroy(gameObject, 3);
         }
-
-        // Destroy the bullet itself on colliding after 3 seconds
         
     }
 
-
-    // Make score tracker reference null
-    public static void NullifyScoreTracker()
-    {
-    }
 }
